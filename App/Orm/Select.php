9ed226d97637636fe841ef8ec54d5392e601a9d5
asinastretche <yochikaffiel@gmail.com>
2024-07-01 22:40:57 +0300

    задание 9.1. Доработка Select: Group By, Order By, Limit

--- App/Orm/Select.php
+++ App/Orm/Select.php
@@ -5,7 +5,11 @@ use PDO;
 class Select
 {
     private string|array $field = '*' ;
-    private string|array $tableName;
+    private string|array $tableName = '';
+    private string|array $groupBy = '';
+    private string|array $orderBy = '';
+    private string|array $limit = '';
+    private string|array $offset = '';
     private PDO $connect;
 
     public function __construct()
@@ -35,9 +39,59 @@ class Select
         $this->tableName = $tableName;
     }
 
+    public function getGroupBy()
+    {
+        if (!is_array($this->groupBy)){
+            return $this ->groupBy;
+        }
+    }
+    public function setGroupBy($groupBy): void
+    {
+        $this->groupBy = $groupBy;
+    }
+
+    public function getOrderBy()
+    {
+        if (is_array($this->orderBy)){
+            $orderItem = [];
+            foreach ($this->orderBy as $key => $value) {
+                $orderItem[] = $key. " " . $value;
+            }
+            return implode(", ", $orderItem);
+        } else {
+            return $this->orderBy;
+        }
+    }
+    public function setOrderBy($orderBy): void
+    {
+        $this->orderBy = $orderBy;
+    }
+
+    public function getLimit()
+    {
+        return $this ->limit;
+    }
+    public function setLimit($limit): void
+    {
+        $this->limit = $limit;
+    }
+
+
     public function build():string
     {
-        return "SELECT ".$this->getField()." FROM ". $this->getTableName();
+     $sql=  "SELECT ".$this->getField().
+            " FROM ". $this->getTableName();
+     if (!empty($this->getGroupBy())){
+         $sql .= " GROUP BY ". $this->getGroupBy();
+     }
+     if (!empty($this->getOrderBy())){
+         $sql .= " ORDER BY ". $this->getOrderBy();
+     }
+     if (!empty($this->getLimit())){
+        $sql .= " LIMIT ". $this->getLimit();
+     }
+
+     return $sql;
     }
 
     public function execute() :?array
